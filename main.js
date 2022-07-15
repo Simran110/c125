55da6df89aa8b77823b0a3ca92117c8d94a14b2e
Simran110 <79517905+Simran110@users.noreply.github.com>
2022-07-15 19:53:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function setup() {
+    video = createCapture(VIDEO)
+    video.size(550, 500)
+    canvas = createCanvas(550, 550)
+    canvas.position(560, 150)
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose",gotPoses)
+}
+noseX = 0
+noseY = 0
+
+difference = 0
+rightWristX = 0
+leftWristX = 0
+
+function draw() {
+    background("skyblue")
+    fill("coral")
+    stroke("purple")
+    square(noseX, noseY, difference)
+    document.getElementById("square_side").innerHTML = "Width and Height of the square will be = " + difference + " px";
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized")
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        noseX = results[0].pose.nose.x
+        noseY = results[0].pose.nose.y
+        console.log("noseX = " + noseX + " noseY = " + noseY)
+        leftWristX = results[0].pose.leftWrist.x
+        rightWristX = results[0].pose.rightWrist.x
+        difference = floor(leftWristX - rightWristX)
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX+" difference = "+difference);
+    }
+}
